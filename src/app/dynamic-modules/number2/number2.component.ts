7138a87b53c43ff0bb6eff321c59aba9de2fa010
Michael Cohen <michael.cohen@buildasign.com>
2018-01-22 11:09:22 -0600

    create two additional components to be dynamically loaded

---
+++ src/app/dynamic-modules/number2/number2.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-number2',
+  templateUrl: './number2.component.html',
+  styleUrls: ['./number2.component.scss']
+})
+export class Number2Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/dynamic-modules/thirdcomponent/thirdcomponent.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-thirdcomponent',
+  templateUrl: './thirdcomponent.component.html',
+  styleUrls: ['./thirdcomponent.component.scss']
+})
+export class ThirdcomponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
